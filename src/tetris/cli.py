97943cfd136ba7c92e2d304d3b8f97c15d5f1ed1
jayzhu <jay.l.zhu@foxmail.com>
2026-03-02 20:56:19 +0800

    use multiple windows for display and change the preview and hold display

--- src/tetris/cli.py
+++ src/tetris/cli.py
@@ -1,25 +1,19 @@
 import curses
 import sys
 
-from tetris import GAME_WINDOW_SIZE_HEIGHT, GAME_WINDOW_SIZE_WIDTH, Tetris
+from tetris import Tetris
 
 
 def wrapper(stdscr: curses.window) -> int:
-    if curses.COLS < GAME_WINDOW_SIZE_WIDTH or curses.LINES < GAME_WINDOW_SIZE_HEIGHT:
+    try:
+        Tetris(stdscr).main()
+    except:
         return 1
-    Tetris(stdscr).main()
     return 0
 
 
 def main() -> int:
-    if curses.wrapper(wrapper) == 1:
-        print(
-            f"ensure your terminal has at least {GAME_WINDOW_SIZE_HEIGHT} rows and {GAME_WINDOW_SIZE_WIDTH} columns."
-        )
-        print("To ensure the game runs smoothly.")
-        return 1
-    return 0
-
+    return curses.wrapper(wrapper)
 
 if __name__ == "__main__":
     sys.exit(main())
