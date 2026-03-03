0f9963e5af93f2403f2f0e856b4da33920c6e0fb
jayzhu <jay.l.zhu@foxmail.com>
2026-03-03 23:10:00 +0800

    fix lockdown logic

--- src/tetris/cli.py
+++ src/tetris/cli.py
@@ -1,19 +1,41 @@
 import curses
 import sys
+import argparse
+
+from importlib.metadata import PackageNotFoundError, version
 
 from tetris import Tetris
 
 
-def wrapper(stdscr: curses.window) -> int:
+def get_version() -> str:
     try:
-        Tetris(stdscr).main()
-    except:
-        return 1
+        return version("tetris-terminal")
+    except PackageNotFoundError:
+        return "0.0.0-dev"
+
+
+def wrapper(stdscr: curses.window) -> int:
+    Tetris(stdscr).main()
     return 0
 
 
 def main() -> int:
+    parser = argparse.ArgumentParser(
+        description="Tetris Terminal",
+        epilog=f"Tetris Terminal v{get_version()}",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"v{get_version()}"
+    )
+
+    args = parser.parse_args()
+
     return curses.wrapper(wrapper)
+
 
 if __name__ == "__main__":
     sys.exit(main())
